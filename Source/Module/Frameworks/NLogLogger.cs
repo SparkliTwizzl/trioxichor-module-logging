a277ba47cbe7af66bf6b1663e58c9943dcaf3299
SparkliTwizzl <github@sparklitwizzl.com>
2025-11-08 00:35:47 -0700

    add blank lines between methods in framework logger wrapper classes

--- Source/Module/Frameworks/NLogLogger.cs
+++ Source/Module/Frameworks/NLogLogger.cs
@@ -7,33 +7,62 @@ public class NLogLogger : ILogger
     public NLogLogger( NLog.Logger logger ) => Logger = logger;
 
     public void Debug( string message ) => Logger.Debug( message );
+
     public void Debug( string message, params object[] args ) => Logger.Debug( message, args );
+
     public void Debug( Exception exception, string message ) => Logger.Debug( exception, message );
+
     public void Debug( Exception exception, string message, params object[] args ) => Logger.Debug( exception, message, args );
+
     public void Debug( Func<string> messageFactory ) => Logger.Debug( messageFactory );
+
     public void Error( string message ) => Logger.Error( message );
+
     public void Error( string message, params object[] args ) => Logger.Error( message, args );
+
     public void Error( Exception exception, string message ) => Logger.Error( exception, message );
+
     public void Error( Exception exception, string message, params object[] args ) => Logger.Error( exception, message, args );
+
     public void Error( Func<string> messageFactory ) => Logger.Error( messageFactory );
+
     public void Fatal( string message ) => Logger.Fatal( message );
+
     public void Fatal( string message, params object[] args ) => Logger.Fatal( message, args );
+
     public void Fatal( Exception exception, string message ) => Logger.Fatal( exception, message );
+
     public void Fatal( Exception exception, string message, params object[] args ) => Logger.Fatal( exception, message, args );
+
     public void Fatal( Func<string> messageFactory ) => Logger.Fatal( messageFactory );
+
     public void Info( string message ) => Logger.Info( message );
+
     public void Info( string message, params object[] args ) => Logger.Info( message, args );
+
     public void Info( Exception exception, string message ) => Logger.Info( exception, message );
+
     public void Info( Exception exception, string message, params object[] args ) => Logger.Info( exception, message, args );
+
     public void Info( Func<string> messageFactory ) => Logger.Info( messageFactory );
+
     public void Trace( string message ) => Logger.Trace( message );
+
     public void Trace( string message, params object[] args ) => Logger.Trace( message, args );
+
     public void Trace( Exception exception, string message ) => Logger.Trace( exception, message );
+
     public void Trace( Exception exception, string message, params object[] args ) => Logger.Trace( exception, message, args );
+
     public void Trace( Func<string> messageFactory ) => Logger.Trace( messageFactory );
+
     public void Warning( string message ) => Logger.Warn( message );
+
     public void Warning( string message, params object[] args ) => Logger.Warn( message, args );
+
     public void Warning( Exception exception, string message ) => Logger.Warn( exception, message );
+
     public void Warning( Exception exception, string message, params object[] args ) => Logger.Warn( exception, message, args );
+
     public void Warning( Func<string> messageFactory ) => Logger.Warn( messageFactory );
 }
--- Source/Module/Frameworks/log4netLogger.cs
+++ Source/Module/Frameworks/log4netLogger.cs
@@ -3,33 +3,61 @@ namespace SparkliTwizzl.Trioxichor.Logging.Frameworks;
 public class log4netLogger : ILogger
 {
     public void Debug( string message ) => throw new NotImplementedException();
+
     public void Debug( string message, params object[] args ) => throw new NotImplementedException();
+
     public void Debug( Exception exception, string message ) => throw new NotImplementedException();
+
     public void Debug( Exception exception, string message, params object[] args ) => throw new NotImplementedException();
+
     public void Debug( Func<string> messageFactory ) => throw new NotImplementedException();
+
     public void Error( string message ) => throw new NotImplementedException();
+
     public void Error( string message, params object[] args ) => throw new NotImplementedException();
+
     public void Error( Exception exception, string message ) => throw new NotImplementedException();
+
     public void Error( Exception exception, string message, params object[] args ) => throw new NotImplementedException();
     public void Error( Func<string> messageFactory ) => throw new NotImplementedException();
+
     public void Fatal( string message ) => throw new NotImplementedException();
+
     public void Fatal( string message, params object[] args ) => throw new NotImplementedException();
+
     public void Fatal( Exception exception, string message ) => throw new NotImplementedException();
+
     public void Fatal( Exception exception, string message, params object[] args ) => throw new NotImplementedException();
+
     public void Fatal( Func<string> messageFactory ) => throw new NotImplementedException();
+
     public void Info( string message ) => throw new NotImplementedException();
+
     public void Info( string message, params object[] args ) => throw new NotImplementedException();
+
     public void Info( Exception exception, string message ) => throw new NotImplementedException();
+
     public void Info( Exception exception, string message, params object[] args ) => throw new NotImplementedException();
+
     public void Info( Func<string> messageFactory ) => throw new NotImplementedException();
+
     public void Trace( string message ) => throw new NotImplementedException();
+
     public void Trace( string message, params object[] args ) => throw new NotImplementedException();
+
     public void Trace( Exception exception, string message ) => throw new NotImplementedException();
+
     public void Trace( Exception exception, string message, params object[] args ) => throw new NotImplementedException();
+
     public void Trace( Func<string> messageFactory ) => throw new NotImplementedException();
+
     public void Warning( string message ) => throw new NotImplementedException();
+
     public void Warning( string message, params object[] args ) => throw new NotImplementedException();
+
     public void Warning( Exception exception, string message ) => throw new NotImplementedException();
+
     public void Warning( Exception exception, string message, params object[] args ) => throw new NotImplementedException();
+
     public void Warning( Func<string> messageFactory ) => throw new NotImplementedException();
 }
