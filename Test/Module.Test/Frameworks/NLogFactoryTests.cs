c71f51f1c57eae141841949491aebf0587484923
SparkliTwizzl <github@sparklitwizzl.com>
2025-11-08 07:19:39 -0700

    switch to Xunit for tests

--- Test/Module.Test/Frameworks/NLogFactoryTests.cs
+++ Test/Module.Test/Frameworks/NLogFactoryTests.cs
@@ -1,49 +1,87 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using System.Collections.Generic;
+using NLog;
+using NLog.Targets;
+using SparkliTwizzl.Trioxichor.Logging.Frameworks;
+using Xunit;
 
-namespace SparkliTwizzl.Trioxichor.Logging.Frameworks.Test;
+namespace SparkliTwizzl.Trioxichor.Logging.Test.Frameworks;
 
-[TestClass]
 public class NLogFactoryTests
 {
-    [TestMethod]
-    public void DefaultConstructor_ShouldApplyDefaultConfiguration()
+    [Fact]
+    public void CreateLogger_ShouldReturnNLogLogger()
     {
         var factory = new NLogFactory();
-        var expected = new LogConfiguration();
-        var actual = factory.Configuration;
-        Assert.IsNotNull( actual );
-        Assert.AreEqual( expected, actual );
+        var categoryName = "TestCategory";
+        var logger = factory.CreateLogger( categoryName );
+        Assert.NotNull( logger );
+        _ = Assert.IsType<NLogLogger>( logger );
+        Assert.Equal( categoryName, ( ( NLogLogger ) logger ).CategoryName );
     }
 
-    [TestMethod]
-    public void CustomConstructor_ShouldApplyCustomConfiguration()
+    [Fact]
+    public void Configure_ShouldApplyMinimumLogLevel()
     {
-        var config = new LogConfiguration()
+        var config = new LogConfiguration
         {
-            ConsoleColorMap = new Dictionary<LogLevel, ConsoleColor>
-            {
-                { LogLevel.Info, ConsoleColor.Green },
-                { LogLevel.Warning, ConsoleColor.Yellow },
-                { LogLevel.Error, ConsoleColor.Red }
-            },
-            MinimumLevel = LogLevel.Warning,
-            Targets = new List<LogTarget>
-            {
-                new LogTarget { Type = LogTargetType.Console },
-                new LogTarget { Type = LogTargetType.Json, FilePath = "logs/log.txt" }
-            }
+            MinimumLevel = LogLevel.Warning
         };
         var factory = new NLogFactory( config );
-        var expected = config;
-        var actual = factory.Configuration;
-        Assert.IsNotNull( actual );
-        Assert.AreEqual( expected, actual );
+        var nlogConfig = LogManager.Configuration;
+        Assert.NotNull( nlogConfig );
+        Assert.DoesNotContain( nlogConfig.LoggingRules, rule => rule.Levels.Contains( NLog.LogLevel.Trace ) );
+        Assert.DoesNotContain( nlogConfig.LoggingRules, rule => rule.Levels.Contains( NLog.LogLevel.Debug ) );
+        Assert.DoesNotContain( nlogConfig.LoggingRules, rule => rule.Levels.Contains( NLog.LogLevel.Info ) );
+        Assert.Contains( nlogConfig.LoggingRules, rule => rule.Levels.Contains( NLog.LogLevel.Warn ) );
+        Assert.Contains( nlogConfig.LoggingRules, rule => rule.Levels.Contains( NLog.LogLevel.Error ) );
+        Assert.Contains( nlogConfig.LoggingRules, rule => rule.Levels.Contains( NLog.LogLevel.Fatal ) );
     }
 
-    [TestMethod]
-    public void CreateLogger_ShouldReturnNLogLoggerInstance()
+    [Fact]
+    public void CreateNLogJsonTarget_ShouldCreateValidJsonTarget()
     {
+        var target = new LogTarget
+        {
+            Type = LogTargetType.Json,
+            FilePath = "log.json"
+        };
+        var config = new LogConfiguration
+        {
+            Targets = new List<LogTarget> { target }
+        };
+        _ = new NLogFactory( config );
+        var nlogConfig = LogManager.Configuration;
+        Assert.NotNull( nlogConfig );
+        var allTargets = nlogConfig?.AllTargets;
+        Assert.NotNull( allTargets );
+        if ( allTargets != null )
+        {
+            foreach ( var t in allTargets )
+            {
+                Assert.NotNull( t );
+                if ( t is FileTarget fileTarget )
+                {
+                    Assert.NotNull( fileTarget.FileName );
+                    Assert.Contains( "log.json", fileTarget.FileName.ToString() );
+                }
+            }
+        }
+    }
+
+    [Fact]
+    public void CreateNLogConsoleTarget_ShouldCreateValidConsoleTarget()
+    {
+        var target = new LogTarget
+        {
+            Type = LogTargetType.Console
+        };
+        var config = new LogConfiguration
+        {
+            Targets = new List<LogTarget> { target }
+        };
+        _ = new NLogFactory( config );
+        var nlogConfig = LogManager.Configuration;
+        Assert.NotNull( nlogConfig );
+        Assert.Contains( nlogConfig.AllTargets, t => t is ConsoleTarget );
     }
 }
